0edeffd299e1d33a3ea1ea6b2c2ec7816bc907e2
Alfredo Morales <alfredomorales@Alfredos-MacBook-Pro.local>
2024-02-06 13:18:35 -0500

    adding new edits

--- src/views/Minimalism.tsx
+++ src/views/Minimalism.tsx
@@ -18,15 +18,15 @@ export default function Minimalism():JSX.Element {
           <Container>
             <Grid container justifyContent={'center'} className="fade-in-bottom">
               <Grid item xs={12} mt={5}>
-                <Typography variant='h3' align="center">
+                <Typography variant='h1' align="center" fontSize={35}>
                   A Digital Minimalistic And Abstract Expression 
                 </Typography>
-                <Typography variant='h3' align="center" sx={{mt: 2}}>
+                <Typography variant='h1' align="center" sx={{mt: 2}} fontSize={35}>
                   Printed on Metal
                 </Typography>
               </Grid>
 
-              <Grid xs={12} mt={5}>
+              <Grid item xs={12} mt={4}>
                 <Typography variant='h6' fontSize={15}>
                   In a world inundated with digital noise, a lone artist emerges, embracing the simplicity of digital minimalism. Their canvas: sleek metal, a reflection of modernity and durability. With a deft hand, they craft an abstract expression, each pixel a deliberate stroke in the symphony of simplicity. Void of clutter, yet rich in depth, their creation invites viewers to pause, to contemplate the essence of existence in an increasingly complex digital age.
                 </Typography>
