d06d44da90636dd376df0336ed1d984b09cc856f
Alfredo Morales <alfredomorales@Alfredos-MBP.lan>
2024-02-20 13:15:44 -0500

    adding images to site

--- src/views/Minimalism.tsx
+++ src/views/Minimalism.tsx
@@ -1,8 +1,9 @@
 import { Container, Grid, Typography } from "@mui/material"
+import Carousel from 'react-material-ui-carousel'
 
 import Navbar from "../components/Navbar"
 import '../App.css'
-
+import { MinimalismImages } from "../utils/MinimalismImages"
 
 
 export default function Minimalism():JSX.Element {
@@ -30,10 +31,48 @@ export default function Minimalism():JSX.Element {
                 <Typography variant='h6' fontSize={15}>
                   In a world inundated with digital noise, a lone artist emerges, embracing the simplicity of digital minimalism. Their canvas: sleek metal, a reflection of modernity and durability. With a deft hand, they craft an abstract expression, each pixel a deliberate stroke in the symphony of simplicity. Void of clutter, yet rich in depth, their creation invites viewers to pause, to contemplate the essence of existence in an increasingly complex digital age.
                 </Typography>
+                <Typography variant='h6' fontSize={15} mt={2}>
+                  "In my journey of self growth I've realized I struggle in expressing myself in visual creations. So I made a commitment to push myself by creating a piece everyday over a 30 day period."
+                </Typography>
+                <Typography variant='h6' fontSize={15} mt={2}>
+                  Inspired by the color-blocking themes that he sees represented in his life. Amazia mentions that many of his color choices represent the spectrum of emotions which he went through during his unique painting journey.
+                </Typography>
               </Grid>
             </Grid>
+
+            <Grid container>
+              <Grid item xs={12} marginTop={8} marginBottom={10}>
+                <Carousel
+                  duration={1000}
+                  animation="slide" 
+                  navButtonsAlwaysVisible 
+                  indicators={true}
+                  navButtonsProps={{style: {backgroundColor: "rgba(189, 64, 46, 0.75)" }}}
+                >
+                  {
+                    MinimalismImages.map((item, index) => {
+                      return (
+                        <div key={`imagen-${index}`}>
+                          <img 
+                            src={item} 
+                            alt={`imagen-${index}`} 
+                            style={{
+                              width: "100%", 
+                              borderRadius: 50,
+                              minHeight: 455,
+                              maxHeight: 500, 
+                              objectFit: "contain", 
+                            }}
+                          />
+                        </div>
+                      )
+                    })
+                  }
+                </Carousel>
+              </Grid>
+            </Grid>
+
           </Container>
-        
         </div>
       </div>
     </>
